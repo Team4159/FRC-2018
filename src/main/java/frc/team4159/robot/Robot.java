ee7cd140527d1e7cd8ca5bddb9680b58add2b4a0
Kai Chang <kaijchang@gmail.com>
2019-01-12 14:22:26 -0800

    fix robot for without usb

--- src/main/java/frc/team4159/robot/Robot.java
+++ src/main/java/frc/team4159/robot/Robot.java
@@ -9,20 +9,25 @@ import java.util.logging.Logger;
 
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.RobotController;
+import frc.team4159.robot.subsystems.Drivetrain;
 
 public class Robot extends TimedRobot {
     private Logger batteryLogger;
+    private Drivetrain drivetrain;
+    private OI oi;
 
     @Override
     public void robotInit() {
         try {
             RobotLogger.setup();
+            batteryLogger = Logger.getLogger("team4159.battery");
         } catch (IOException e) {
             e.printStackTrace();
             System.out.println("Problems with creating the log files");
         }
 
-        batteryLogger = Logger.getLogger("team4159.battery");
+        drivetrain = Drivetrain.getInstance();
+        oi = OI.getInstance();
     }
 
     @Override
@@ -32,8 +37,10 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopPeriodic() {
-        if (0.02 * Math.round(Timer.getFPGATimestamp() / 0.02) % 5 == 0) {
-            batteryLogger.info(Double.toString(RobotController.getBatteryVoltage()));
+        if (batteryLogger != null) {
+            if (0.02 * Math.round(Timer.getFPGATimestamp() / 0.02) % 5 == 0) {
+                batteryLogger.info(Double.toString(RobotController.getBatteryVoltage()));
+            }
         }
         
         Scheduler.getInstance().run();
